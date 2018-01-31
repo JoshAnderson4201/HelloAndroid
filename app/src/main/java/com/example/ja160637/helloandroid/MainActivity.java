1d42445a65d12378cd1563f69a0bb2a22e7c5913
Anderson <ja160637@scots.edinboro.edu>
2018-01-30 19:30:40 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ja160637/helloandroid/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.ja160637.helloandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static String MA = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.w(MA, "View resources: " + R.layout.activity_main);
+
+        int count = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            count++;
+        }
+    }
+}
